b307f237921bc9703143bef6a6e93fa66dc598ad
Rathnayaka P.R.M.S.V.B <IT25102829@my.sliit.lk>
2026-05-18 10:32:17 +0530

    Upload User.java

---
+++ backend/src/model/User.java
@@ -0,0 +1,36 @@
+package com.oerms.springboot.model;
+
+public class User {
+
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+    public User() {}
+
+    public User(String id, String name, String email,
+                String password, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+}
