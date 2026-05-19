3c3ca50110ae700eba4385e2216bba4f5bc8c18a
it25101564 <it25101564@my.Sliit.lk>
2026-05-19 11:37:10 +0530

    Update ResultController.java

--- backend/src/controller/ResultController.java
+++ backend/src/controller/ResultController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
+
 
 @CrossOrigin(origins = "*")
 @RestController
